66bc239ef54d9eb0c92dce62ede1616ca7616ce5
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-06-04 18:29:49 -0400

    Shorten lines

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,11 @@ fn trace(ray: &Ray, sphere: &Sphere) -> RGB {
         // calculate color at intersection point
         // TODO(dip): implement recursion, for now only return color
         Some(intersection) => {
-            //
             // uncomment to print intersection locations
-            // eprintln!("[info] found intersection at {} {} {}", intersection.0, intersection.1, intersection.2);
+            // eprintln!("[info] found intersection at {} {} {}",
+            //           intersection.0,
+            //           intersection.1,
+            //           intersection.2);
             let surface_normal = (intersection - sphere.origin).l2_normalize();
             let rescaled_normal = 0.5 * (surface_normal + 1.0);
             return rgb(rescaled_normal.0, rescaled_normal.1, rescaled_normal.2);
@@ -37,7 +39,9 @@ fn trace(ray: &Ray, sphere: &Sphere) -> RGB {
         None => {
             let ray_direction = ray.direction.l2_normalize();
             let height = 0.5 * (ray_direction.1 + 1.0);
-            return rgb((1.0 - height) + height * 0.5, (1.0 - height) + height * 0.7, (1.0 - height) + height * 1.0);
+            return rgb((1.0 - height) + height * 0.5,
+                       (1.0 - height) + height * 0.7,
+                       (1.0 - height) + height * 1.0);
         }
     }
 }
@@ -46,8 +50,11 @@ fn spheres() {
     // construct camera
     let fov = PI / 2.0;
     let aspect_ratio = 16.0 / 9.0;
-    let (image_plane_height, image_plane_width) = fov_to_image_plane_size(fov, aspect_ratio);
-    let (rows, cols, pixel_size) = image_plane_size_to_pixel_shape(image_plane_height, image_plane_width, 2.0 / 480.0);
+    let (image_plane_height, image_plane_width) = fov_to_imsize(fov,
+                                                                aspect_ratio);
+    let (rows, cols, pixel_size) = imsize_to_pixels(image_plane_height,
+                                                    image_plane_width,
+                                                    2.0 / 480.0);
     let camera = Camera {
         origin: Vec3D(0.0, 0.0, 0.0),
         image_plane: ImagePlane {
@@ -67,8 +74,10 @@ fn spheres() {
     // loop over pixels and create rays
     for row in 0..rows {
         for col in 0..cols {
-            // calculate ray for current pixel, making sure to center ray within pixel
-            let ray = camera.ray_from_pixel((row as f64 + 0.5) / (rows as f64), (col as f64 + 0.5) / (cols as f64));
+            // calculate ray for current pixel
+            // making sure to center ray within pixel
+            let ray = camera.ray_from_pixel((row as f64 + 0.5) / (rows as f64),
+                                            (col as f64 + 0.5) / (cols as f64));
             // trace ray for current pixel
             let color = trace(&ray, &big_sphere);
             // add observed color from trace to image at current pixel
