5f738d0b7948e6b78039145c15208e706a92b772
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-06-04 18:04:58 -0400

    Add image output functions

---
+++ src/io.rs
@@ -0,0 +1,17 @@
+pub fn output_ppm(image: Vec<Vec<Vec<f64>>>, rows: usize, cols:usize) {
+    let rows = rows;
+    let cols = cols;
+    println!("P3");
+    print!("{}", cols);
+    print!(" ");
+    println!("{}", rows);
+    println!("{}", 255);
+    for i in 0..rows {
+        for j in 0..cols {
+            let r = (255.999 * image[i][j][0]) as i32;
+            let g = (255.999 * image[i][j][1]) as i32;
+            let b = (255.999 * image[i][j][2]) as i32;
+            println!("{} {} {} ", r, g, b);
+        }
+    }
+}
