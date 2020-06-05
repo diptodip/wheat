06b406b466e204977b527a334e74ba71de1d09a5
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-06-04 22:17:48 -0400

    Use new Intersectable structs and Intersection records

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use io::output_ppm;
 mod geometry;
 use geometry::Ray;
 use geometry::Sphere;
+use geometry::Intersects;
+use geometry::Intersectable;
 
 mod camera;
 use camera::Camera;
@@ -20,20 +22,20 @@ use camera::ImagePlane;
 use camera::fov_to_imsize;
 use camera::imsize_to_pixels;
 
-fn trace(ray: &Ray, sphere: &Sphere) -> RGB {
+fn trace(ray: &Ray, intersectable: &Intersectable) -> RGB {
     // determine if ray intersects
-    let result = sphere.intersects(ray);
+    let result = intersectable.intersects(ray);
     match result {
         // calculate color at intersection point
         // TODO(dip): implement recursion, for now only return color
         Some(intersection) => {
             // uncomment to print intersection locations
             // eprintln!("[info] found intersection at {} {} {}",
-            //           intersection.0,
-            //           intersection.1,
-            //           intersection.2);
-            let surface_normal = (intersection - sphere.origin).l2_normalize();
-            let rescaled_normal = 0.5 * (surface_normal + 1.0);
+            //           intersection.point.0,
+            //           intersection.point.1,
+            //           intersection.point.2);
+            let local_normal = intersection.local_normal;
+            let rescaled_normal = 0.5 * (local_normal + 1.0);
             return rgb(rescaled_normal.0, rescaled_normal.1, rescaled_normal.2);
         },
         None => {
@@ -70,8 +72,10 @@ fn spheres() {
     // construct blank image
     let mut image = vec![vec![vec![0.0; 3]; cols]; rows];
     // construct sphere objects in scene
-    let big_sphere = Sphere {origin: Vec3D(0.0, 0.0, -1.0), radius: 0.5};
+    let big_sphere = Intersectable::Sphere(Sphere {origin: Vec3D(0.0, 0.0, -1.0), radius: 0.5});
     // loop over pixels and create rays
+    eprintln!("[start] processing {}px x {}px (width x height)...", cols, rows);
+    eprintln!("[info] remaining scan lines: {}", rows);
     for row in 0..rows {
         for col in 0..cols {
             // calculate ray for current pixel
