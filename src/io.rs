8347bc10c043872feb367922056fd21c22655227
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-06-05 19:12:45 -0400

    Add gamma correction and clamping

--- src/io.rs
+++ src/io.rs
@@ -8,9 +8,9 @@ pub fn output_ppm(image: Vec<Vec<Vec<f64>>>, rows: usize, cols:usize) {
     println!("{}", 255);
     for i in 0..rows {
         for j in 0..cols {
-            let r = (255.999 * image[i][j][0]) as i32;
-            let g = (255.999 * image[i][j][1]) as i32;
-            let b = (255.999 * image[i][j][2]) as i32;
+            let r = (255.999 * image[i][j][0].sqrt().min(0.999).max(0.0)) as i32;
+            let g = (255.999 * image[i][j][1].sqrt().min(0.999).max(0.0)) as i32;
+            let b = (255.999 * image[i][j][2].sqrt().min(0.999).max(0.0)) as i32;
             println!("{} {} {} ", r, g, b);
         }
     }
