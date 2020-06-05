fc17f74e54d9f58e74c43e83d4477d12146a8754
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-06-04 22:16:51 -0400

    Add Intersects trait, Intersectable enum, and Intersection record struct

--- src/geometry.rs
+++ src/geometry.rs
@@ -13,25 +13,73 @@ impl Ray {
     }
 }
 
+pub struct Intersection {
+    pub point: Vec3D,
+    pub local_normal: Vec3D,
+    pub inside: bool,
+}
+
+pub trait Intersects {
+    fn intersects(&self, ray: &Ray) -> Option<Intersection>;
+    fn surface_normal(&self, point: Vec3D) -> Vec3D;
+}
+
 pub struct Sphere {
     pub origin: Vec3D,
     pub radius: f64,
 }
 
-impl Sphere {
-    pub fn intersects(&self, ray: &Ray) -> Option<Vec3D> {
+impl Intersects for Sphere {
+    fn surface_normal(&self, point: Vec3D) -> Vec3D {
+        (point - self.origin).l2_normalize()
+    }
+
+    fn intersects(&self, ray: &Ray) -> Option<Intersection> {
         // using quadratic formula
         let sphere_to_ray = ray.origin - self.origin;
         let a = dot(&ray.direction, &ray.direction);
         let h = dot(&sphere_to_ray, &ray.direction);
         let c = dot(&sphere_to_ray, &sphere_to_ray) - self.radius * self.radius;
         let discriminant = (h * h) - (a * c);
-        if discriminant > 0.0 {
-            let t = (-h - discriminant.sqrt()) / a;
-            if t > 0.0 {
-                return Some(ray.at(t));
-            }
+        if discriminant < 0.0 {
+            return None;
         }
-        return None;
+        let discriminant_sqrt = discriminant.sqrt();
+        let t0 = (-h - discriminant_sqrt) / a;
+        let t1 = (-h + discriminant_sqrt) / a;
+        if t0 < 0.0 && t1 < 0.0 {
+            return None;
+        }
+        let point = if t0 < t1 { ray.at(t0) } else { ray.at(t1) };
+        let surface_normal = self.surface_normal(point);
+        let mut inside = false;
+        if dot(&ray.direction, &surface_normal) > 0.0 {
+            inside = true;
+        }
+        let local_normal = if inside { -surface_normal } else { surface_normal };
+        let intersection = Intersection {
+            point: point,
+            local_normal: local_normal,
+            inside: inside,
+        };
+        Some(intersection)
+    }
+}
+
+pub enum Intersectable {
+    Sphere(Sphere),
+}
+
+impl Intersects for Intersectable {
+    fn surface_normal(&self, point: Vec3D) -> Vec3D {
+        match self {
+            Intersectable::Sphere(s) => s.surface_normal(point),
+        }
+    }
+
+    fn intersects(&self, ray: &Ray) -> Option<Intersection> {
+        match self {
+            Intersectable::Sphere(s) => s.intersects(ray),
+        }
     }
 }
