4e5f7c3e181bb1ab572140eb2396cc85ef27bb29
Diptodip Deb <d2d.tar.gz@gmail.com>
2020-06-05 21:30:46 -0400

    Add reflective materials

--- src/materials.rs
+++ src/materials.rs
@@ -11,7 +11,14 @@ pub struct Reflective {
 }
 
 #[derive(Copy, Clone)]
+pub struct FuzzyReflective {
+    pub color: RGB,
+    pub fuzz: f64,
+}
+
+#[derive(Copy, Clone)]
 pub enum Material {
     Diffuse(Diffuse),
     Reflective(Reflective),
+    FuzzyReflective(FuzzyReflective),
 }
