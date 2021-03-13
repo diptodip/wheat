bb8e00108a39c1ee139246e791e7da2ece67308c
Diptodip Deb <d2d.tar.gz@gmail.com>
2021-03-13 16:39:08 -0500

    Fix maximum value for pixel output

--- src/io.rs
+++ src/io.rs
@@ -6,11 +6,12 @@ pub fn output_ppm(image: Vec<Vec<Vec<f64>>>, rows: usize, cols:usize) {
     print!(" ");
     println!("{}", rows);
     println!("{}", 255);
+    let cap = 255.999;
     for i in 0..rows {
         for j in 0..cols {
-            let r = (255.999 * image[i][j][0].sqrt().min(0.999).max(0.0)) as i32;
-            let g = (255.999 * image[i][j][1].sqrt().min(0.999).max(0.0)) as i32;
-            let b = (255.999 * image[i][j][2].sqrt().min(0.999).max(0.0)) as i32;
+            let r = (cap * image[i][j][0].sqrt().min(0.999).max(0.0)) as i32;
+            let g = (cap * image[i][j][1].sqrt().min(0.999).max(0.0)) as i32;
+            let b = (cap * image[i][j][2].sqrt().min(0.999).max(0.0)) as i32;
             println!("{} {} {} ", r, g, b);
         }
     }
