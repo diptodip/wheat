e085d52f9418d0eb34b44cdae4e7f8179a91468a
Diptodip Deb <dd@d2d.sh>
2021-06-11 12:46:20 -0400

    Operate on a flat buffer instead of 2D

--- src/io.rs
+++ src/io.rs
@@ -1,18 +1,16 @@
-pub fn output_ppm(image: Vec<Vec<Vec<f64>>>, rows: usize, cols: usize) {
-    let rows = rows;
-    let cols = cols;
+pub fn output_ppm(image: Vec<Vec<f64>>, rows: usize, cols: usize) {
     println!("P3");
     print!("{}", cols);
     print!(" ");
     println!("{}", rows);
     println!("{}", 255);
     let cap = 255.999;
-    for i in 0..rows {
-        for j in 0..cols {
-            let r = (cap * image[i][j][0].sqrt().min(0.999).max(0.0)) as i32;
-            let g = (cap * image[i][j][1].sqrt().min(0.999).max(0.0)) as i32;
-            let b = (cap * image[i][j][2].sqrt().min(0.999).max(0.0)) as i32;
-            println!("{} {} {} ", r, g, b);
-        }
+    for i in 0..rows*cols {
+        let row = i / cols;
+        let col = i % rows;
+        let r = (cap * image[i][0].sqrt().min(0.999).max(0.0)) as i32;
+        let g = (cap * image[i][1].sqrt().min(0.999).max(0.0)) as i32;
+        let b = (cap * image[i][2].sqrt().min(0.999).max(0.0)) as i32;
+        println!("{} {} {} ", r, g, b);
     }
 }
