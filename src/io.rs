e403b03430177dbfc8a2c2a966814ef36fd00afd
Diptodip Deb <dd@d2d.sh>
2021-06-11 19:25:48 -0400

    Write file directly from Rust

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,13 @@
-pub fn output_ppm(image: Vec<Vec<f64>>, rows: usize, cols: usize) {
-    println!("P3");
-    print!("{}", cols);
-    print!(" ");
-    println!("{}", rows);
-    println!("{}", 255);
+use std::fs::{self, File};
+use std::io::{Error, Write};
+
+pub fn write_ppm(image: Vec<Vec<f64>>, rows: usize, cols: usize) -> Result<(), Error>{
+    let mut f = File::create("image.ppm")?;
+    writeln!(f, "P3");
+    write!(f, "{}", cols);
+    write!(f, " ");
+    writeln!(f, "{}", rows);
+    writeln!(f, "{}", 255);
     let cap = 255.999;
     for i in 0..rows*cols {
         let row = i / cols;
@@ -11,6 +15,7 @@ pub fn output_ppm(image: Vec<Vec<f64>>, rows: usize, cols: usize) {
         let r = (cap * image[i][0].sqrt().min(0.999).max(0.0)) as i32;
         let g = (cap * image[i][1].sqrt().min(0.999).max(0.0)) as i32;
         let b = (cap * image[i][2].sqrt().min(0.999).max(0.0)) as i32;
-        println!("{} {} {} ", r, g, b);
+        writeln!(f, "{} {} {} ", r, g, b);
     }
+    Ok(())
 }
