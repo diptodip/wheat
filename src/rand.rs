abe2eeb26a6b7a8f47cbc83737caa888fc4d8ea4
dd@d2d.sh <dd@d2d.sh>
2025-04-25 16:52:11 -0400

    Remove temp variable

--- src/rand.rs
+++ src/rand.rs
@@ -5,13 +5,12 @@ pub struct PRNG {
 
 impl PRNG {
     pub fn xor_shift32(&mut self) -> u32 {
-        let mut x = self.state;
-        x ^= x << 13;
-        x ^= x >> 17;
-        x ^= x << 5;
-        self.state = x;
-        return x;
+        self.state ^= self.state << 13;
+        self.state ^= self.state >> 17;
+        self.state ^= self.state << 5;
+        return self.state;
     }
+
     pub fn warm_up_xor_shift(&mut self) {
         for _ in 0..11 {
             let _x = self.xor_shift32();
